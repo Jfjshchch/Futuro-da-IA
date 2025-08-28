7170ef46079adaeb8c2377ba8a1bc4d6edeac8d2
Careca <163576366+Jfjshchch@users.noreply.github.com>
2025-08-28 16:57:46 -0300

    ....

---
+++ script.js
@@ -0,0 +1,7 @@
+const caixaPerguntas = document.querySelector(".caixa-pergunta");
+
+const caixaAlternativas = document.querySelector(".caixa-alternativa");
+
+const caixaResultado = document.querySelector(".caixa-resultado");
+
+const caixaResultado =document.querySelector(".texto-resultafdo");
