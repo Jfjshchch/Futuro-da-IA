41227d1e82b6c2bd992315e908b443f145ddf953
Careca <163576366+Jfjshchch@users.noreply.github.com>
2025-08-28 17:17:02 -0300

    ...

--- script.js
+++ script.js
@@ -4,4 +4,30 @@ const caixaAlternativas = document.querySelector(".caixa-alternativa");
 
 const caixaResultado = document.querySelector(".caixa-resultado");
 
-const caixaResultado =document.querySelector(".texto-resultafdo");
+const caixaResultado =document.querySelector(".texto-resultado");
+
+const perguntas = [
+{
+ enunciado:
+  "Como a IA afetará o mercado de trabalho no futuro?"
+ 
+  alternativas: [
+     "A IA pode auxiliar na analise de grandes volumes de dados medicos, aprimorando diagnostico e tratamento personalizados."
+
+     "substituirá completamente os medicos"
+ 
+  ]
+
+},
+{  
+    enunciado: "Como a IA afetará o trabalho?"
+
+    alternativas:[
+       "substituirá algumas funções, mas criará novas oportunidades."
+
+       "substituirá todos os empregos humanos."
+    ]
+}
+{
+    
+}
