ec22152cbd5aed14c25ffd5abdab004405a8d9b9
Careca <163576366+Jfjshchch@users.noreply.github.com>
2025-09-11 16:47:24 -0300

    ...

--- script.js
+++ script.js
@@ -57,3 +57,21 @@ const perguntas = [
  }
 
  mostraPergunta();
+  let atual = 0;
+  let perguntaAtual;
+
+  function mostraPergunta(){
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+  }
+
+  function mostraAlternativas(){
+    for (const) alternativa of perguntaAtual.alternativas){
+      const botaoAlternativas = document.createElement("button");
+      botaoAlternativas.textContent = alternativas;
+      caixaAlternativas.appendChild(botaoAlternativas);
+    }
+  }
+
+  mostraPergunta();
